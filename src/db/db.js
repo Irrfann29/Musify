63d63459214a7d869c380cb9232c821865ef5923
Irrfann29 <mrirfanhashmi2303@gmail.com>
2026-07-23 07:26:32 +0530

    Initial Commit

---
+++ src/db/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+async function connectDB(){
+    try{
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("connected to DB");
+        
+    }
+
+    catch(err){
+        message : "Database Connection Error", err
+
+    }
+}
+module.exports = connectDB;
